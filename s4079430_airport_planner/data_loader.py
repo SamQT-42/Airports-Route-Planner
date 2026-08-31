f40dd0454656dec8193a5fcb4dd857730009a5f4
Sam.QT-42 <s4079430@rmit.edu.vn>
2026-08-31 14:22:04 +0700

    [FEAT] added data_loader.py with some simple data cleaning, any routes that does not have sources or destinations' airporys, and any airports without IATA will get dropped

--- s4079430_airport_planner/data_loader.py
+++ s4079430_airport_planner/data_loader.py
@@ -1,7 +1,7 @@
 #clean the data, drop contents that does not satisfy the format ID/Airport/FROM/TO/iata/longtitude/latitude
 
 import csv
-#mapping, skip airports with no IATA code
+#mapping, skip airports with no IATA, and skip routes without airports
 class Airports:
     def __init__(self, iata, name, country, latitude, longtitude):
         self.iata = iata
@@ -28,3 +28,20 @@ def load_airports(filepath):
             airports[iata] = Airports(iata, name, country, latitude, longtitude)
     return airports
 
+def load_routes(filepath, airports):
+    graph = {code:set() for code in airports}
+    with open(filepath, newline="", encoding="utf-8") as f:
+        reader = csv.reader(f)
+        for row in reader:
+            if not row:
+                continue
+            source = row[2]
+            destination = row[4]
+            if source in airports and destination in airports:
+                graph[source].add(destination)
+    return graph
+
+def load_network(airports_filepath, routes_filepath):
+    airports = load_airports(airports_filepath)
+    graph = load_routes(routes_filepath, airports)
+    return airports, graph
